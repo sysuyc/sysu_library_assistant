124d15439d22e0a53f1472e9ee283e127ccdd25d
yizhikong <343802694@qq.com>
2015-05-17 18:40:36 +0800

    Revert "finish all APIs without debug"

--- sysu_library/library/models.py
+++ sysu_library/library/models.py
@@ -3,22 +3,24 @@ from django.db import models
 # Create your models here.
 
 class Courses(models.Model):
-	cname = models.CharField(max_length = 100)
-	description = models.CharField(max_length = 2000)
-	def __unicode__(self):
-		return "Course name : " + cname
+    cname = models.CharField(max_length = 100)
+    description = models.CharField(max_length = 2000)
+    def __unicode__(self):
+        return "Course name : " + cname
 
 class Books(models.Model):
-	bname = models.CharField(max_length = 100)
-	isbn = models.CharField(max_length = 20)
-	author = models.CharField(max_length = 100)
-	publisher = models.CharField(max_length = 100)
-	pic = models.CharField(max_length = 100)
-	url = models.CharField(max_length = 60)
-	def __unicode__(self):
-		return "Book name : " + bname + "\nauthor : " + author + "\npublisher : " + publisher
+    bname = models.CharField(max_length = 100)
+    isbn = models.CharField(max_length = 20)
+    author = models.CharField(max_length = 100)
+    publisher = models.CharField(max_length = 100)
+    pic = models.CharField(max_length = 100)
+    url = models.CharField(max_length = 60)
+    def __unicode__(self):
+        return "Book name : " + bname + "\nauthor : " + author + "\npublisher : " + publisher
 
 class Relation(models.Model):
-	bid = models.IntegerField(default =-0)
-	cid = models.IntegerField(default = 0)
-	click = models.IntegerField(default = 0)
+    # one course has several correlated books
+    course = models.ForeignKey(Courses)
+    # not every book has relation with course, eg : search book directly by book name
+    bid = models.IntegerField(default = 0)
+    click = models.IntegerField(default = 0)
