f7eb47cd4c3ba051546f39ff882cd828c4f876c5
yizhikong <343802694@qq.com>
2015-06-15 16:08:29 +0800

    use redis

--- sysu_library/sysu_library/settings.py
+++ sysu_library/sysu_library/settings.py
@@ -67,6 +67,16 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.cache.RedisCache',
+        'LOCATION': '127.0.0.1:6379',
+        'OPTION': {
+            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+        },
+    }
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
